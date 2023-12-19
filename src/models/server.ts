2bb554f96a455eaa00ec9b39b4adb852ca272bf1
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2023-12-19 06:45:23 -0500

    metodo routes creado en la clase Server

--- src/models/server.ts
+++ src/models/server.ts
@@ -1,19 +1,30 @@
 import express, { Application } from "express"
 import cors from "cors"
+import users from "../routes/users.routes"
 
 class Server {
     private app: Application
     private port: number
+    private path = {
+        users : '/api/users'
+    }
     constructor() {
         this.app = express()
         this.port = Number(process.env.PORT)
 
         this.middlewares()
+
+
+        this.routes()
     }
 
     middlewares() {
         this.app.use(cors())
         this.app.use(express.json())
+    }
+
+    routes() {
+        this.app.use(this.path.users, users)
     }
 
     listen() {
