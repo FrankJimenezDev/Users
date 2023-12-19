a84ec2cc437976bf74ef32a747c6ed16097f0e5a
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2023-12-19 08:16:29 -0500

    metodos getAllUsers y getOneUser configurados y funcionales

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,15 +1,59 @@
 import { Request, Response, } from "express"
+import { User } from "../config/entities/user.entity"
 
 
 export class UsersController {
-    getAllUsers(req: Request, res: Response) {
-        res.json({
-            msg: `getAllUsers`,
-            response: `Todos los Usuarios`
-        })
+    async getAllUsers(req: Request, res: Response) {
+
+        try {
+            const usuarios: User[] = await User.find()
+            if (usuarios.length === 0) {
+                res.status(404).json({
+                    msg: `No se encontraron Usuarios`
+                })
+                return;
+            }
+
+            res.status(200).json({
+                msg: `getAllUsers`,
+                usuarios
+            })
+        } catch (error) {
+            res.status(500).json({
+                msg: `Ha ocurrido un error por favor contacte con el administrado`,
+                error
+            })
+        }
+
     }
 
-    getOneUser(req: Request, res: Response) {
+    async getOneUser(req: Request, res: Response) {
+
+        const { id } = req.params
+
+        try {
+            const usuario = await User.findOneBy({
+                id
+            })
+
+            if (!usuario) {
+                res.status(404).json({
+                    msg: `No se encontro usuario con id: ${id}`
+                })
+                return;
+            }
+
+            res.status(200).json({
+                msg: `Usuario encontrado`,
+                usuario
+            })
+        } catch (error) {
+            res.status(500).json({
+                msg: `Ha ocurrido un error por favor contacte con el administrado`,
+                error
+            })
+        }
+
         res.json({
             msg: `getOneUser`,
             response: `obtener usuario`
