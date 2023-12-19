a545328847f426511526513068001616d76875d3
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2023-12-19 06:23:51 -0500

    cors es integrado en el metodo middlewares

--- src/models/server.ts
+++ src/models/server.ts
@@ -1,4 +1,5 @@
 import express, { Application } from "express"
+import cors from "cors"
 
 class Server {
     private app: Application
@@ -11,6 +12,7 @@ class Server {
     }
 
     middlewares() {
+        this.app.use(cors())
         this.app.use(express.json())
     }
 
