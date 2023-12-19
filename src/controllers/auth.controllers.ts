7d036cd197e868a8316fc09cfa47fb555f3cf604
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2023-12-19 10:15:07 -0500

    archivo auth.controllers.ts modificado, devuleve el token correctamente

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,14 +1,49 @@
 import { Request, Response } from "express";
+import { db } from "../config/db/connection";
+import { User } from "../config/entities/user.entity";
+import { compare } from "bcryptjs";
+import { generarJWT } from "../helpers/jwt-generator";
 
 
 export class AuthController {
 
-    
-    authUser(req: Request, res: Response) {
-      
 
-        res.json({
-            msg: `AuthUser`
-        })
+    async authUser(req: Request, res: Response) {
+        const { email, password } = req.body
+        const userDB = db.getRepository(User)
+
+        try {
+
+            const usuario = await userDB.findOneBy({
+                email
+            })
+
+            if (!usuario) {
+                res.status(404).json({
+                    msg: `Usuario o Contraseña invalidos`
+                })
+                return;
+            }
+
+            const checkPassword = await compare(password, usuario.password)
+
+            if (!checkPassword) {
+                res.status(404).json({
+                    msg: `Usuario o Contraseña invalidos`
+                })
+                return;
+            }
+
+            const token = await generarJWT(usuario)
+
+            res.status(200).json({
+                token
+            })
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                msg: 'hubo un error comuniquese con el administrador'
+            })
+        }
     }
 }
