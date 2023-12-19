b91c0c40e20138623b85f735876b6d9c233c3cc3
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2023-12-19 07:09:10 -0500

    metodos de UsersController creados, getOneUser, updateUser, createUser ydeleteUser, falta configurar

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,10 +1,39 @@
-import { Request, Response,  } from "express"
+import { Request, Response, } from "express"
 
 
 export class UsersController {
-    getAllUsers(req: Request, res : Response) {
+    getAllUsers(req: Request, res: Response) {
         res.json({
-            msg: `hola mundo`
+            msg: `getAllUsers`,
+            response: `Todos los Usuarios`
+        })
+    }
+
+    getOneUser(req: Request, res: Response) {
+        res.json({
+            msg: `getOneUser`,
+            response: `obtener usuario`
+        })
+    }
+
+    updateUser(req: Request, res: Response) {
+        res.json({
+            msg: `updateUser`,
+            response: `usuario actualizado`
+        })
+    }
+
+    createUser(req: Request, res: Response) {
+        res.json({
+            msg: `createUser`,
+            response: `crear usuario`
+        })
+    }
+
+    deleteUser(req: Request, res: Response) {
+        res.json({
+            msg: `getAllUsers`,
+            response: `Todos los Usuarios`
         })
     }
 }
