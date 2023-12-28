4a83463c3dbe77319d34b522e850b45c4e483caf
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2023-12-27 23:11:09 -0500

    metodo logout agregado al archivo auth.controller.ts

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken"
+
 import { db } from "../config/db/connection";
 import { User } from "../config/entities/user.entity";
 import { compare } from "bcryptjs";
@@ -37,7 +39,7 @@ export class AuthController {
             }
 
             const kafkaMessage = await userDB.findOne({
-                where:{
+                where: {
                     email
                 },
                 select: {
@@ -52,7 +54,7 @@ export class AuthController {
             const runProducer = async () => {
                 try {
                     await kafkaProducer.connect();
-                    await kafkaProducer.sendMessage('test', 'your-key', JSON.stringify(kafkaMessage));
+                    await kafkaProducer.sendMessage('test', 'logIn', JSON.stringify(kafkaMessage));
                 } catch (error) {
                     console.error('Error:', error);
                 } finally {
@@ -74,6 +76,60 @@ export class AuthController {
             res.status(500).json({
                 msg: 'hubo un error comuniquese con el administrador'
             })
+        }
+    }
+
+    async logoutUser(req: Request, res: Response) {
+
+        const token = req.cookies['token']
+
+        const secreto = 'Pr0j3kt0Fr4nc08'
+
+        const payload : any = jwt.verify(token, secreto);
+
+        const email = payload.email
+        
+
+        // const userDB = db.getRepository(User)
+
+        try {
+
+            const kafkaMessage = {
+                email,
+                fecha: new Date()
+            };
+
+            // Puedes agregar lógica adicional aquí según tus requisitos
+            // Por ejemplo, puedes invalidar el token, limpiar la sesión, etc.
+
+            // Enviar un mensaje de cierre de sesión a Kafka si es necesario
+            const kafkaProducer = new KafkaProducer(['localhost:9092'], 'MyKafkaIdClient');
+            const runProducer = async () => {
+                try {
+                    await kafkaProducer.connect();
+                    // Puedes enviar un mensaje específico para indicar el cierre de sesión
+                    await kafkaProducer.sendMessage('test', 'logOut', JSON.stringify(kafkaMessage));
+                } catch (error) {
+                    console.error('Error:', error);
+                } finally {
+                    await kafkaProducer.disconnect();
+                }
+            };
+
+            // Ejecuta el productor
+            runProducer();
+
+            // Limpiar la cookie de token
+            res.clearCookie('token');
+
+            res.status(200).json({
+                msg: 'Sesión cerrada exitosamente',
+            });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({
+                msg: 'Hubo un error. Comuníquese con el administrador',
+            });
         }
     }
 }
